ac07c5b47421cbffb1d2bd4e3ec083d77147afaa
Felipe Cardozo <dafelcardozo@gmail.com>
2023-08-06 14:09:08 -0500

    Buttons alignment.

--- components/home/content-slide.tsx
+++ components/home/content-slide.tsx
@@ -1,21 +1,23 @@
-import { Button } from '@mui/material';
-import { useDispatch, useSelector} from 'react-redux';
-import {registrationSlice} from '@/components/shared/registrationSlice';
-import {ReduxState} from '@/components/shared/store';
+import { Button, Box } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { registrationSlice } from '@/components/shared/registrationSlice';
+import { ReduxState } from '@/components/shared/store';
+import { PropsWithChildren } from 'react';
 
-
-export type ContentSlideProps = {
-    children: any
-}
-
-export default function ContentSlide({ children}:ContentSlideProps) {
-    const {hasPrevious, hasNext } = useSelector((state:ReduxState) => state.register);
+export default function ContentSlide({ children }: PropsWithChildren) {
+    const { hasPrevious, hasNext } = useSelector((state: ReduxState) => state.register);
     const dispatch = useDispatch();
     return (<div>
         <div>
             {children}
         </div>
-        { hasPrevious && <Button variant="contained" onClick={() => dispatch(registrationSlice.actions.goPrevious(''))}>Go back</Button>}
-        { hasNext && <Button variant="contained" onClick={() => dispatch(registrationSlice.actions.goNext(''))}>Next Step</Button>}
+        <Box component="span"
+            m={1}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center">
+            <div><Button variant="contained" onClick={() => dispatch(registrationSlice.actions.goPrevious(''))} sx={{ display: hasPrevious ? undefined : 'none' }}>Go back</Button></div>
+            <div><Button variant="contained" onClick={() => dispatch(registrationSlice.actions.goNext(''))} sx={{ display: hasNext ? undefined : 'none' }}>Next Step</Button></div>
+        </Box>
     </div>);
 }
