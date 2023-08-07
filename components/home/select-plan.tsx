4efe4a83bfe7bd62ef69435d7aa43783bb170923
Felipe Cardozo <dafelcardozo@gmail.com>
2023-08-06 20:32:00 -0500

    Plan selection updates.

--- components/home/select-plan.tsx
+++ components/home/select-plan.tsx
@@ -4,7 +4,7 @@ import { FormControl, ToggleButtonGroup, ToggleButton, Switch, FormControlLabel,
 import ArcadeIcon from '../shared/icons/arcade-icon';
 import AdvancedIcon from '../shared/icons/advanced';
 import ProIcon from '../shared/icons/pro-icon';
-import { planSelectionSlice } from '../shared/planSlice';
+import { setIsYearly, setPlan } from '../shared/planSlice';
 import { ReduxState } from '../shared/store';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
@@ -43,16 +43,16 @@ export default function SelectYourPlan() {
         <FormControl>
             <Typography variant="h1">Select your plan</Typography>
             <div>You have the option of monthly or yearly billing.</div>
-            <ToggleButtonGroup color="primary"
+            <ToggleButtonGroup color="primary" 
                 value={plan}
                 exclusive
-                onChange={(e, selection) => dispatch(planSelectionSlice.actions.setPlan(selection))}
+                onChange={(e, selection) => dispatch(setPlan(selection))}
                 aria-label="Platform">
                 <ToggleButton value="arcade"><ArcadeButton /></ToggleButton>
                 <ToggleButton value="advanced"><AdvancedButton /></ToggleButton>
                 <ToggleButton value="pro"><ProButton /></ToggleButton>
             </ToggleButtonGroup>
-            <FormControlLabel control={<Switch checked={is_yearly} onChange={(e, checked) => dispatch(planSelectionSlice.actions.setIsYearly(checked))}/>} label="Yearly" />
+            <FormControlLabel sx={{backgroundColor:'#F8F9FF'}} control={<Switch checked={is_yearly} onChange={(e, checked) => dispatch(setIsYearly(checked))}/>} label="Yearly" />
         </FormControl>
         </Box>);
 }
