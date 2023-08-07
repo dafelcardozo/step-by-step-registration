d8e53e6df63e673576773dc7233bdf0f5b5b7258
Felipe Cardozo <dafelcardozo@gmail.com>
2023-08-06 19:55:52 -0500

    Way improved add-ons.

--- components/home/pick-add-ons.tsx
+++ components/home/pick-add-ons.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControl, Checkbox, FormControlLabel, Typography, Container } from "@mui/material";
+import { Box, FormControl, Checkbox, FormControlLabel, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { setCustomizableProfile, setLargerStorage, setOnlineService } from "../shared/addOnsSlice";
 import { ReduxState } from "../shared/store";
@@ -10,7 +10,7 @@ type AddOnCardProps = {
     description: string
 }
 
-function AddOnCard({title, price, description}: AddOnCardProps) {
+function AddOnCard({ title, price, description }: AddOnCardProps) {
     return (<>
         <div>{title}</div>
         <Typography align="right">{price}</Typography>
@@ -19,47 +19,42 @@ function AddOnCard({title, price, description}: AddOnCardProps) {
 }
 
 function OnlineServiceLabel() {
-    return <AddOnCard title="Online service" price="+$1/mo" description="Access to multiplayer games"/>
+    return <AddOnCard title="Online service" price="+$1/mo" description="Access to multiplayer games" />
 }
 
 function LargerStorageLabel() {
-    return <AddOnCard title="Larger storage" price="+$2/mo" description="Extra 1TB of cloud save"/>
+    return <AddOnCard title="Larger storage" price="+$2/mo" description="Extra 1TB of cloud save" />
 }
 
 function CustomizableProfile() {
-    return (<div>
-        <div>Customizable Profile</div>
-        <div>+2/mo</div>
-        <div>Custom theme on your profile</div>
-    </div>);
+    return <AddOnCard title="Customizable Profile" price="+2/mo" description="Custom theme on your profile"/>
+}
+
+function labelStyles(isChecked: boolean) {
+    return {
+        padding: '20px',
+        border: '1px solid ' + (isChecked ? '#483EFF' : '#D6D9E6'),
+        borderRadius: '8px',
+        marginBottom: '16px',
+        backgroundColor: '#F8F9FF'
+    };
 }
 
 export default function PickAddOns() {
     const { onlineService, largerStorage, customizableProfile } = useSelector((state: ReduxState) => state.addOns);
     const dispatch = useDispatch();
-    const labelStyles = {
-        padding: '20px',
-        border: '1px solid #D6D9E6',
-        borderRadius: '8px',
-        marginBottom:'10px',
-        '&.Mui-checked': {
-            backgroundColor:'red',
-            color:'yellow',
-            borderColor:'red'
-         },
-       }
     const checkBoxStyles = {
         '&.Mui-checked': {
-            color:'#483EFF'
-         },
-       };
+            color: '#483EFF'
+        },
+    };
     return (<Box>
-            <h1>Pick add-ons</h1>
-            <div>Add-ons help enhance your gaming experience.</div>
-        <FormControl sx={{padding:'5px'}}>
-            <FormControlLabel  sx={labelStyles} control={<Checkbox value={onlineService} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setOnlineService(checked))} />} label={<OnlineServiceLabel />} />
-            <FormControlLabel sx={labelStyles} control={<Checkbox value={largerStorage} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setLargerStorage(checked))} />} label={<LargerStorageLabel />} />
-            <FormControlLabel sx={labelStyles} control={<Checkbox value={customizableProfile} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setCustomizableProfile(checked))} />} label={<CustomizableProfile />} />
+        <Typography variant="h1">Pick add-ons</Typography>
+        <div>Add-ons help enhance your gaming experience.</div>
+        <FormControl sx={{ padding: '5px' }}>
+            <FormControlLabel sx={labelStyles(onlineService)} control={<Checkbox checked={onlineService} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setOnlineService(checked))} />} label={<OnlineServiceLabel />} />
+            <FormControlLabel sx={labelStyles(largerStorage)} control={<Checkbox checked={largerStorage} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setLargerStorage(checked))} />} label={<LargerStorageLabel />} />
+            <FormControlLabel sx={labelStyles(customizableProfile)} control={<Checkbox checked={customizableProfile} sx={checkBoxStyles} onChange={(e, checked) => dispatch(setCustomizableProfile(checked))} />} label={<CustomizableProfile />} />
         </FormControl>
     </Box>);
 }
